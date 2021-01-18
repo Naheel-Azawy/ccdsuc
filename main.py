418816b9f70738ff22264fb18f3afef7edb59c7d
Naheel-Azawy <naheelazawy@gmail.com>
2021-01-18 13:58:29 +0300

    structured dirs

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+from sys import argv
+
+def help():
+    print("usage: thething COMMAND [ARGS]")
+    print("COMMANDS:")
+    print("  iot-server     start the sample TCP server")
+
+if __name__ == "__name__":
+    help()
